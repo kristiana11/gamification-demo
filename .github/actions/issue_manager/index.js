53dde2ab7cbb5a6a81b64a9ddf21f851b5f330ca
Kristiana Kirk <93414377+kristiana11@users.noreply.github.com>
2024-03-21 11:02:52 -0700

    issue_manager index.js
    This is the index.js file for the issue_manager

--- .github/actions/issue_manager/index.js
+++ .github/actions/issue_manager/index.js
@@ -1,3 +1,4 @@
+// main code for managing Issues. Main functions are create and delete
 const github = require('@actions/github');
 
 try{
@@ -12,11 +13,37 @@ try{
     switch (action) {
         case 'create':
             // create issue
-
+            const response = await octokit.rest.issues.create({
+                owner: github.context.repo.owner,
+                repo: github.context.repo.repo,
+                title: description,
+                body: description
+            });
+            console.log('Issue created successfully:', response.data.html_url);
         break;
+
         case 'delete':
             // delete issue
+        const issues = await octokit.rest.issues.listForRepo({
+            owner: github.context.repo.owner,
+            repo: github.context.repo.repo,
+            state: 'open',
+            creator: user
+        });
+        if (issues.data.length > 0) {
+            await octokit.rest.issues.update({
+                owner: github.context.repo.owner,
+                repo: github.context.repo.repo,
+                issue_number: issues.data[0].number,
+                state: 'closed'
+            });
+            console.log('Issue deleted');
+        } 
+        else {
+            console.log('No open issue found for the specified user');
+        }
         break;
+
       default:
         console.log('Invalid action specified');
     }
@@ -24,4 +51,3 @@ try{
 catch (error) {
     console.log(error.message);
 }
-    }
